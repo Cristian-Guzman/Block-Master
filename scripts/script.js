8a2cc55544c2e809fb0a20c83e888544cc0e02c0
Cristian Guzman <cristiangt1813@gmail.com>
2021-10-22 15:20:51 -0500

    Agregando los cards

---
+++ scripts/script.js
@@ -0,0 +1,50 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+const IMG_PATH = `https://image.tmdb.org/t/p/w1280`
+const SEARCH_URL = 'http://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
+const cards = document.querySelector('.cards');
+
+document.addEventListener('DOMContentLoaded', () => {
+    getData();
+})
+
+const getData = async() => {
+    let respuesta = await fetch(API_URL);
+    let data = await respuesta.json();
+    let {results} = data;
+    showData(results);
+}
+const showData = async(data) => {
+    data.map(datos => {
+        let {title, vote_average, release_date, poster_path, overview} = datos;
+        console.log(release_date);
+        // cardImg.innerHTML = ``;
+        let card = document.createElement('DIV');
+        let infoCard = `
+        <div class="card__puntuacion"></div>
+        <div class="card__img"><img src="${IMG_PATH + poster_path}">
+        </div>`;
+        card.innerHTML = infoCard;
+        /* let cardPuntuacion = document.createElement('DIV').classList.add('card__puntuacion');
+        let cardImg = document.createElement('DIV').classList.add('card__img'); */
+        let fragment = document.createDocumentFragment();
+        fragment.appendChild(card);
+        cards.appendChild(fragment);
+    })
+}
+
+
+/* 
+let texto = document.getElementById('inputBuscar').value;
+
+    let data = await getData();
+    let busqueda = data.filter(hero => hero.superhero.toLowerCase() ===  texto.toLowerCase())
+    busqueda.forEach(heroe => {
+        let {superhero,image} = heroe;
+        templateCard.querySelector('h5').textContent = superhero;
+ templateCard.querySelector('img').setAttribute('src',image);
+        const clone = templateCard.cloneNode(true);
+        fragment.appendChild(clone);    
+    });
+    items.innerHTML = "";
+    items.appendChild(fragment);
+     */
